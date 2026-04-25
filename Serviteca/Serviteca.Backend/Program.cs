5c99f0606440e31fa26b72fcc8b6ca6a506a526e
janier2000 <jomr10@hotmail.com>
2026-04-25 18:54:59 -0500

    customer 25-04-26

--- Serviteca/Serviteca.Backend/Program.cs
+++ Serviteca/Serviteca.Backend/Program.cs
@@ -1,15 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Serviteca.Backend.Data;
+using Serviteca.Backend.Repositories.Implementations;
+using Serviteca.Backend.Repositories.Interface;
+using Serviteca.Backend.UnitsOfWork.Implementations;
+using Serviteca.Backend.UnitsOfWork.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
+
+builder.Services.AddTransient<SeedDb>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
+
+
+
+builder.Services.AddScoped<ICustomersRepository, CustomersRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// inyection manual Tutorial 72 - Parte 19 - Alimentador de base de datos https://www.youtube.com/watch?v=VD1b8yAMC7o&list=PLuEZQoW9bRnRBThyGs208ZMrCYBRTvIg2&index=19 
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory!.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
+    }
+}
+
+//para seguridad
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
